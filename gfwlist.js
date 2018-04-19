66971b391b9c94ed6149647184bdb4fe4e08ddbd
王镇 <HyperSimon@users.noreply.github.com>
2018-04-19 23:40:53 +0800

    Update gfwlist.js

--- gfwlist.js
+++ gfwlist.js
@@ -4,6 +4,8 @@
 var proxy = "SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT;";
 
 var rules = [
+  "||gate.io",
+  "||okex.com",
   "||wakatime.com",
   "facebook.github.io",
   "||reactnavigation.org", 
